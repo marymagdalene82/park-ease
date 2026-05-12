18d580d26626e88bea3000c1beabc41bbae284e6
Mary Magdalene <mary.achero@students.mak.ac.ug>
2026-05-12 07:57:38 +0300

    Created a user model

--- config/parkease/models.py
+++ config/parkease/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+# User Model
+class User(AbstractUser):
+
+    ROLE_CHOICES = [
+        ('attendant', 'Parking Attendant'),
+        ('tyre_manager', 'Tyre Manager'),
+        ('battery_manager', 'Battery Manager'),
+        ('admin', 'System Admin'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
