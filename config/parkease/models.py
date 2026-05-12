dcb9ef37b401e623233ddbdaca1f581dbc4a1bca
Mirembe Jane Mugeni <mirembejanegift@gmail.com>
2026-05-12 11:34:00 +0300

    vehicle_registration

--- config/parkease/models.py
+++ config/parkease/models.py
@@ -18,3 +18,22 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+
+
+class Vehicle_registration(models.Model):
+    driver_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    Vehicle_type = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    nin = models.CharField(max_length=20, blank=True, null=True)  
+    arrival_time = models.TimeField()
+    arrival_date = models.DateField()
+    receipt_number = models.CharField(max_length=50, unique=True)  
+    is_signed_out = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="vehicles")
+    
+    def __str__(self):
+        return self.name
